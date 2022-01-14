c9902d8335b8f507779cb3545ce23f6623a6fff1
cjw6k <chris@lot2k.com>
2022-01-14 11:42:05 -0400

    initial development

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.1-alpine
+FROM php:7.4-cli-alpine
 
 ARG PHPMD_VERSION=2.11.1
 
@@ -6,7 +6,7 @@ RUN wget -P /usr/local/bin -q https://github.com/phpmd/phpmd/releases/download/$
  && chmod +x /usr/local/bin/phpmd.phar
 
 RUN apk --no-cache add git bash \
- && rm -rf /var/lib/apt/lists/* \
+ && rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-cd "$GITHUB_WORKSPACE"
+cd "${GITHUB_WORKSPACE}"
+
+if [ ${#INPUT_PHPMD_SOURCE_DIFF_FROM_BRANCH} -eq 0 ]; then
+  filesToCheck=${INPUT_PHPMD_SOURCE_ARG}
+else
+  filesToCheck=$(git diff --name-only ${INPUT_PHPMD_SOURCE_DIFF_FROM_BRANCH} | grep php$)
+  if [ ${#filesToCheck} -eq 0 ]; then
+    echo "There are no changed files to check."
+    exit 0
+  fi
+  printf -v filesToCheck "%s," $filesToCheck
+fi
+
+/usr/local/bin/php \
+  ${INPUT_PHP_ARGS} \
+  /usr/local/bin/phpmd.phar \
+    ${filesToCheck%,} \
+    github \
+    ${INPUT_PHPMD_RULESET_ARG} \
+    --suffixes php \
+    --reportfile /tmp/phpmd_result.gh \
+    ${INPUT_PHPMD_OPTIONAL_ARGS}
+
+EXIT_CODE=$?
+
+# Two issues going on here:
+#  i) the warnings issued may correspond to lines that have not been modified
+#  ii) the exit code may change if warnings for non-changed lines are disregarded
+
+cat /tmp/phpmd_result.gh
+
+exit $EXIT_CODE
